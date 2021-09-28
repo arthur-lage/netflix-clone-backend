610f88810817ff820f5ee68f6098f39977bfd447
Arthur Lage <arthurlage2006@gmail.com>
2021-09-28 20:33:20 -0300

    register created

--- api/account.js
+++ api/account.js
@@ -1,12 +1,31 @@
 const express = require("express")
 const router = express.Router()
 
+const Account = require("../model/Account")
+
 router.get("/", (req, res) => {
-    res.send("Hello")
+    let existentAccounts = Account.find()
+    res.send(existentAccounts)
 })
 
 router.post("/register", (req, res) => {
-    
+    const name = req.body.name
+    const email = req.body.email
+    const password = req.body.password
+
+    const newUser = {
+        name: name,
+        email: email,
+        password: password
+    }
+
+    Account.create(newUser, function (err, result) {
+        if(err){
+            console.log(err)
+        } else {
+            res.send("Account created successfully")
+        }
+    })
 })
 
 router.post("/login", (req, res) => {
--- index.js
+++ index.js
@@ -3,6 +3,16 @@ const app = express()
 const mongoose = require("mongoose")
 const account = require("./api/account")
 
+require("dotenv").config()
+
+const dbPassword = process.env.DB_PASSWORD
+const mongooseURI = `mongodb+srv://admin:${dbPassword}@cluster0.8l9zr.mongodb.net/netflix-clone?retryWrites=true&w=majority`
+
+mongoose.connect(mongooseURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).catch(e => console.log(e))
+
 const PORT = process.env.PORT || 8080
 
 app.use(express.urlencoded({ extended: true }))
