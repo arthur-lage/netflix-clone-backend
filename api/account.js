50e0d147a146d15f65468c64db49474dd4b0ddc5
Arthur Lage <arthurlage2006@gmail.com>
2021-09-28 18:30:03 -0300

    test get route

--- api/account.js
+++ api/account.js
@@ -1,6 +1,10 @@
 const express = require("express")
 const router = express.Router()
 
+router.get("/", (req, res) => {
+    res.send("Hello")
+})
+
 router.post("/register", (req, res) => {
     
 })
