24d8dfdb46fe7ebeac150e270dd6b7469525a33b
Arthur Lage <arthurlage2006@gmail.com>
2021-09-29 08:21:27 -0300

    login system added

--- api/account.js
+++ api/account.js
@@ -10,30 +10,55 @@ router.get("/", async (req, res) => {
 })
 
 router.post("/register", (req, res) => {
-    const name = req.body.name
-    const email = req.body.email
-    const password = req.body.password
-
-    const salt = 10
-
-    bcrypt.hash(password, salt, (err, hash) => {
-        if(err){
-            console.log(err)
-            res.send(err)
-        } else {
-            Account.create({name: name, email: email, password: hash}, function (err, result) {
-                if(err){
-                    console.log(err)
-                } else {
-                    res.send(result)
-                }
-            })
-        }
-    })
+    try {
+        const name = String(req.body.name).charAt(0).toUpperCase() + String(req.body.name).slice(1)
+        const email = String(req.body.email).toLowerCase()
+        const password = req.body.password
+    
+        const salt = 10
+    
+        bcrypt.hash(password, salt, (err, hash) => {
+            if(err){
+                console.log(err)
+                res.send(err)
+            } else {
+                Account.create({name: name, email: email, password: hash}, function (err, result) {
+                    if(err){
+                        console.log(err)
+                    } else {
+                        res.send(result)
+                    }
+                })
+            }
+        })
+    } catch (e) {
+        res.send("Couldn't register account.")
+        console.log(e)
+    } 
 })
 
-router.post("/login", (req, res) => {
-    
+router.post("/login", async (req, res) => {
+    try {
+        const email = req.body.email
+        const password = req.body.password
+
+        const account = await Account.find({ email })
+
+        bcrypt.compare(password, account[0].password, (err, result) => {
+            if(err){
+                console.log(err)
+            } else {
+                if(result == false){
+                    res.send("Email e/ou senha incorretos")
+                } else {
+                    res.send("Deu certooo")
+                }
+            }
+        })
+    } catch (e) {
+        res.send("Couldn't login.")
+        console.log(e)
+    }
 })
 
 router.delete("/delete", async (req, res) => {
